5fc3bd662ec82f475748c10746b8bdbb6cf660c4
Manjeet Dahiya <manjeetdahiya@gmail.com>
2017-01-04 16:37:30 +0530

    inline

--- cint/sense.c
+++ cint/sense.c
@@ -105,58 +105,41 @@ unsigned divide (unsigned numerator, unsigned denominator) {
     return (numerator / denominator);
 }
 
-void compute (void) {
-    unsigned i, sampval;
-    unsigned min, max, mean, stddev, diff;
-    long sumdiffs;
-
-    min = samples[0];
-    max = samples[0];
-    mean = samples[0];
-
-    for (i = 1; i < NUMSAMPLES; ++i) {
-        sampval = samples[i];
-        if (sampval < min) min = sampval;
-        if (sampval > max) max = sampval;
-
-        // running average
-        // mean = (((i-1) * mean) + sampval) / i;
-        mean = divide(((i-1) * mean) + sampval, i);
-#ifndef __MSP430__
-        printf("mean = %d\n", mean);
-#endif // __MSP430__
-    }
-
-    sampmean = mean;
-    sampmax = max;
-    sampmin = min;
-
-    sumdiffs = 0;
-    for (i = 0; i < NUMSAMPLES; ++i) {
-        diff = (samples[i] - mean);
-        sumdiffs += diff * diff;
-    }
-    sampstddev = fast_sqrt(sumdiffs / NUMSAMPLES);
-#ifndef __MSP430__
-    printf("sampstddev = %d\n", sampstddev);
-#endif // __MSP430__
+#define compute() {\
+    unsigned i, sampval;\
+    unsigned min, max, mean, stddev, diff;\
+    long sumdiffs;\
+    min = samples[0];\
+    max = samples[0];\
+    mean = samples[0];\
+    for (i = 1; i < NUMSAMPLES; ++i) {\
+        sampval = samples[i];\
+        if (sampval < min) min = sampval;\
+        if (sampval > max) max = sampval;\
+        mean = divide(((i-1) * mean) + sampval, i);\
+    }\
+    sampmean = mean;\
+    sampmax = max;\
+    sampmin = min;\
+    sumdiffs = 0;\
+    for (i = 0; i < NUMSAMPLES; ++i) {\
+        diff = (samples[i] - mean);\
+        sumdiffs += diff * diff;\
+    }\
+    sampstddev = fast_sqrt(sumdiffs / NUMSAMPLES);\
 }
 
 #define SEG15 0xFE00
-void store (void) {
-    /* put stuff in flash segment 15 */
-//    FCTL3 = FWKEY;
-//    FCTL1 = FWKEY + WRT;
-    MEMREF(SEG15) = sampmin;
-    MEMREF(SEG15+2) = sampmax;
-    MEMREF(SEG15+4) = sampmean;
-    MEMREF(SEG15+6) = sampstddev;
-//    FCTL1 = FWKEY;
-//    FCTL3 = FWKEY + LOCK;
+//void store (void) {
+#define store() {   \
+    MEMREF(SEG15) = sampmin;\
+    MEMREF(SEG15+2) = sampmax;\
+    MEMREF(SEG15+4) = sampmean;\
+    MEMREF(SEG15+6) = sampstddev;\
 }
 
 //MEMENTOS_MAIN_ATTRIBUTES
-int main (void) {
+int main_sense (void) {
 #ifdef __MSP430__
     WDTCTL = WDTPW + WDTHOLD; // stop WDT
     setup();
@@ -167,7 +150,12 @@ int main (void) {
     printf("sampmean = %d\n", sampmean);
     printf("sampmin = %d\n", sampmin);
     printf("sampmax = %d\n", sampmax);
+    int a = sampmean + sampmin + sampmax;
 #endif // __MSP430__
     store();
-    return 91;
+    return a;
+}
+
+int main() {
+  return main_sense();
 }
