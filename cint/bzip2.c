dd3290c49f3b5218271b7d93978d435f67c86db7
Sorav Bansal <sbansal@iitd.ac.in>
2019-09-28 18:50:39 +0530

    bzip2 : more DBGs

--- cint/bzip2.c
+++ cint/bzip2.c
@@ -4830,6 +4830,7 @@ int main (int argc, char *argv[]) {
   }
   /* Save off one byte every ~1k for validation */
   for (i = 0; i*VALIDATE_SKIP < input_size*MB; i++) {
+    DBG(__LINE__);
 	  validate_array[i] = spec_fd[0].buf[i*VALIDATE_SKIP];
   }
 
@@ -4881,6 +4882,7 @@ int main (int argc, char *argv[]) {
 #endif
 
 	  for (i = 0; i*VALIDATE_SKIP < input_size*MB; i++) {
+      DBG(__LINE__);
 	    if (validate_array[i] != spec_fd[0].buf[i*VALIDATE_SKIP]) {
 		    printf ("Tested %dMB buffer: Miscompared!!\n", input_size);
 		    MYmyexit (1);
